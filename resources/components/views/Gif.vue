95dabeaeddf2e47255b0e36cce4436ffe69cc446
Louay Hamada <loauy.hamada@gmail.com>
2020-04-02 01:25:45 +0300

    Implement Navigation through Stored Results

--- resources/components/views/Gif.vue
+++ resources/components/views/Gif.vue
@@ -1,5 +1,13 @@
 <template>
-    <section id="single-gif">
+    <section id="single-gif" v-if="gif">
+        <div id="navigation">
+            <div id="prev" class="arrow" v-if="gifIndex > 0" @click="changeGif(-1)">
+                <span class="icon left-arrow"></span> Previous
+            </div>
+            <div id="next" class="arrow" v-if="gifIndex < 23" @click="changeGif(1)">
+                Next <span class="icon right-arrow"></span>
+            </div>
+        </div>
         <video controls :src="gif.images.original_mp4.mp4"></video>
         <div id="actions">
             <div id="favorite" class="action">
@@ -15,9 +23,27 @@
 <script>
 export default {
     props: {
-        gif: {
+        openedGif: {
             type: Object,
             required: true
+        }
+    },
+    data: () => ({
+        gif: null
+    }),
+    computed: {
+        gifIndex() {
+            return this.$store.getters.results.findIndex(result => result.id === this.gif.id);
+        }
+    },
+    methods: {
+        changeGif(offset) {
+            this.gif = this.$store.getters.results[this.gifIndex + offset];
+        }
+    },
+    mounted() {
+        if(this.openedGif) {
+            this.gif = this.openedGif;
         }
     }
 }
@@ -41,6 +67,7 @@ export default {
 
         .action {
             display: flex;
+            align-items: center;
 
             .icon {
                 display: inline-block;
@@ -62,6 +89,46 @@ export default {
                 &.heart {
                     mask: url('../../images/heart.svg');
                     background: #e74c3c;
+                }
+            }
+        }
+    }
+
+    #navigation {
+        display: flex;
+        justify-content: space-between;
+        margin-bottom: 1rem;
+        color: #ecf0f1;
+
+        .arrow {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+
+            &#prev {
+                margin-right: auto;
+            }
+
+            &#next {
+                margin-left: auto;
+            }
+
+            .icon {
+                display: inline-block;
+                width: .8rem;
+                height: 1rem;
+                mask-size: cover;
+
+                &.left-arrow {
+                    margin-right: .5rem;
+                    mask: url('../../images/left-arrow.svg');
+                    background: #ecf0f1;
+                }
+
+                &.right-arrow {
+                    margin-left: .5rem;
+                    mask: url('../../images/right-arrow.svg');
+                    background: #ecf0f1;
                 }
             }
         }
