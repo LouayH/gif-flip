2f46a6b35ea0f7962b9bfbacc99936f881964a00
Louay Hamada <loauy.hamada@gmail.com>
2020-04-01 03:34:27 +0300

    Add "vuex" Package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import router from './router';
+import store from './store';
 import Layout from '../components/Layout';
 
 new Vue({
     router,
+    store,
     render: h => h(Layout)
 }).$mount('#app');
